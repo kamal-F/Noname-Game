10cc0cd4784496b8cfb85e14e452e14e2e5e8e4c
kamal fauzan <kamalonnetx@gmail.com>
2018-02-09 22:39:05 +0700

    images shrink

--- Thanks.java
+++ Thanks.java
@@ -17,6 +17,6 @@ public class Thanks extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(960, 540, 1);
-        setBackground("winbg.png");
+        setBackground("winbg.jpg");
     }
 }
