ee30b4300e6f25ce16d871555fa66f8351a01773
kamal fauzan <kamalonnetx@gmail.com>
2018-02-21 00:45:07 +0700

    minor

--- About.java
+++ About.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class About extends Tombol
 {
+    public About(){
+        setImage("button_about.png");
+    }
     /**
      * Act - do whatever the About wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
